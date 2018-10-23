de0f06b02ad87a4ac90bec778da50c76e02ff115
K <zzim2x@gmail.com>
2018-10-23 11:14:20 +0900

    fix test

--- test/unit/specs/lib.spec.js
+++ test/unit/specs/lib.spec.js
@@ -7,13 +7,13 @@ describe('Wallet&Sebak library', function () {
     const seed = 'SAU2GZJXASXDAB5DI4ZI2H6SJO27UIRXW6LMGOWWC7RGULVMAW4TVIVK';
     const address = 'GCNOVP44M34SDWFQAQQ2RER6CQ72IVEOMGTF55UCCJX55PTYMORMWL2T';
     const account = wallet.createFreezeAccount(seed, 1);
-    const tx = wire.createFreezeAccountTx(address, 10000000000, 10000, 1, account.publicKey());
+    const tx = wire.createFreezeAccountTx(address, 100000000000, 10000, 1, account.publicKey());
 
     return wallet.hash(tx.nestedArrays()).then((hash) => {
-      assert.equal(hash, '5Dj7PZqGNx5ZxuMZu83eDSpctDLZhDUtbKWyVY7SdjgR');
+      assert.equal(hash, '5FvwzQBd5w5o2fQLtZcasQmDiQi3agmT4HNogdjV7FTJ');
       assert.equal(
         wallet.sign(seed, hash),
-        '2b7EhdJT9ukdVXT15Jczb3QHsKvfXiuhcuR1oxRMnLstx93BPw5s6UChz2nN3cNNXSDLippqJ6GF26CtigS1tUe3',
+        'StkHsNP6YqDBh3q8fAMCLZfCfAyXXuLK31cQS7yKPxeM3fYxK77jgYN2Jh9DZ6jzJNJ2zoB5CBxrJhmzMEr5gAk',
       );
     });
   });
